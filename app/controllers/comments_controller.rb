278b6410c3f26c2fbdf787d715e011803097dfcc
ZeinabElshorbagy <zeinabelshorbagy@gmail.com>
2018-10-06 00:02:01 +0200

    basic authantcations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  
   def index
   end
 
@@ -22,6 +24,10 @@ class CommentsController < ApplicationController
   end
 
   def destory
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   def comment_params
